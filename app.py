bb6e26ec7fee9de99030265bbc8dcb858a541a8c
Eric Xia <ericpingxia@gmail.com>
2025-06-01 12:34:53 -0400

    update app serve 5000

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = create_app()
 # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 if __name__ == "__main__": 
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=5000)
     # app.run(host='localhost', port=8080, debug=True, use_reloader=False)
     
--- j.bat
+++ j.bat
@@ -1,6 +1,6 @@
 rem this is a quickstart script that opens localhost:5000 and the server
 set FLASK_APP=app.py
 set FLASK_DEBUG=1
-start "" http://localhost:5000/
-flask run --port=5000
+start "" http://localhost:8080/
+flask run --port=8080
 cmd /k
