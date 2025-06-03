7400c7b87b42f7d2dad90b8e52b9aefc84db4901
Julian Beaudry <julianbeaudry@Julians-MacBook-Pro.local>
2025-06-03 13:10:06 -0400

    recent shifting change

--- application/static/util/help.js
+++ application/static/util/help.js
@@ -8,30 +8,81 @@ function toggleHelp() {
     }
 
 }
+
+// // I am going to edit this help 
+// function openHelp() {
+//     if(document.getElementById("help") === null){
+//     const help = document.createElement("div");
+//     const help_container = document.createElement("div");
+//     help.append(help_container)
+//     help.className = "help"
+//     help.id = "help"
+//     help_container.className = "help_container"
+//     help_container.innerHTML =  
+//     ` 
+//     <h1>How to play</h1>
+//     <div class='help-text'>
+//     <p> Get to a target word by clicking on semantic neighbors. </p>
+//     <p class='--grayed-out'> cement ⟶ lumber ⟶ forests ⟶ woodlands </p>
+//     <p>As you get closer, the words will get more similar. </p>
+//     <p class='--grayed-out'> alphabet ⟶ rhyme ⟶ lyrical ⟶ duet ⟶ drummer ⟶ performer ⟶ newcomer ⟶ sophomore </p>
+//     <p>Each target can be reached in two jumps!  </p>
+//     <p class='--grayed-out'> fountain ⟶ piers ⟶ coastline </p>
+//     </div>
+//     <button onclick='closeHelp()' class='switch switch--outlined'> play </button>
+//     `;
+//     document.body.appendChild(help);
+//     }
+// }
+
 function openHelp() {
-    if(document.getElementById("help") === null){
-    const help = document.createElement("div");
-    const help_container = document.createElement("div");
-    help.append(help_container)
-    help.className = "help"
-    help.id = "help"
-    help_container.className = "help_container"
-    help_container.innerHTML =  
-    ` 
-    <h1>How to play</h1>
-    <div class='help-text'>
-    <p> Get to a target word by clicking on semantic neighbors. </p>
-    <p class='--grayed-out'> cement ⟶ lumber ⟶ forests ⟶ woodlands </p>
-    <p>As you get closer, the words will get more similar. </p>
-    <p class='--grayed-out'> alphabet ⟶ rhyme ⟶ lyrical ⟶ duet ⟶ drummer ⟶ performer ⟶ newcomer ⟶ sophomore </p>
-    <p>Each target can be reached in two jumps!  </p>
-    <p class='--grayed-out'> fountain ⟶ piers ⟶ coastline </p>
-    </div>
-    <button onclick='closeHelp()' class='switch switch--outlined'> play </button>
-    `;
-    document.body.appendChild(help);
+    if (document.getElementById("help") === null) {
+        const help = document.createElement("div");
+        help.className = "help";
+        help.id = "help";
+
+        const helpContainer = document.createElement("div");
+        helpContainer.className = "help_container";
+
+        // Array of slides (as HTML strings)
+        const slides = [
+            `<div class="help-slide">
+                <h1>How to play</h1>
+                <p>Get to a target word by clicking on semantic neighbors.</p>
+                <p class='--grayed-out'>cement ⟶ lumber ⟶ forests ⟶ woodlands</p>
+            </div>`,
+            `<div class="help-slide">
+                <p>As you get closer, the words will get more similar.</p>
+                <p class='--grayed-out'>alphabet ⟶ rhyme ⟶ lyrical ⟶ duet ⟶ drummer ⟶ performer ⟶ newcomer ⟶ sophomore</p>
+            </div>`,
+            `<div class="help-slide">
+                <p>Each target can be reached in two jumps!</p>
+                <p class='--grayed-out'>fountain ⟶ piers ⟶ coastline</p>
+            </div>`,
+            `<div class="help-slide" style="text-align:center;">
+                <button onclick='closeHelp()' class='switch switch--outlined'>Play</button>
+            </div>`
+        ];
+
+        let currentSlide = 0;
+        helpContainer.innerHTML = slides[currentSlide];
+        help.appendChild(helpContainer);
+        document.body.appendChild(help);
+
+        // Start slideshow
+        const interval = setInterval(() => {
+            console.log("here is the slide count");
+            console.log(currentSlide);
+            currentSlide++;
+            if (currentSlide >= slides.length) {
+                clearInterval(interval);
+                return;
+            }
+            helpContainer.innerHTML = slides[currentSlide];
+        }, 5000); // 1000 ms = 1 second
     }
 }
+
 
 
 function closeHelp(){
