78ea7124fef23b4a586fe0dd609ce650d69cdb6b
Eric Xia <ericpingxia@gmail.com>
2025-06-18 10:33:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = create_app()
 
 # Disable caching for static files (after updating data model)
 # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-#commettd
+#commettdf
 if __name__ == "__main__": 
     # serve(app, host="0.0.0.0", port=8000)
     app.run(host='localhost', port=8080, debug=True, use_reloader=False)
