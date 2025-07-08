9b3c687cca2523dce0da0a39a12e4e23080e28b4
Eric Xia <ericpingxia@gmail.com>
2025-07-08 11:09:04 -0400

    [UI, Prog] finish-help-guest

--- application/static/js/information.js
+++ application/static/js/information.js
@@ -175,8 +175,6 @@ function generateLineGraph(scores) {
 
 function displayFinishModal(daily_idx, totalJumps, currentStreak, jumpsGridMessage, is_user=false) {
     const modalFinish = document.getElementById(is_user ? 'modal-finish-user' : 'modal-finish-guest');
-    modalFinish.querySelector('.daily-idx').innerHTML = daily_idx;
-    modalFinish.querySelector('.daily-idx').innerHTML = daily_idx;
     modalFinish.querySelector('.totalJumps').innerHTML = totalJumps;
     modalFinish.querySelector('.streak').innerHTML = currentStreak;
     modalFinish.style.display = "flex";
