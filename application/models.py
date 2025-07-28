599965be13f731ef8d35e45aaa5e7d6b8f94d0f8
jb2700 <113483299+jb2700@users.noreply.github.com>
2025-07-28 11:03:18 -0400

    [Database Migration] new create prompt gamestate

--- application/models.py
+++ application/models.py
@@ -35,6 +35,23 @@ class GameState(db.Model):
         db.UniqueConstraint("user_id", "current_date", name="user_date_uc"),
     )
 
+class FakeGameState(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(36), db.ForeignKey("user.id"), nullable=False)
+    current_date = db.Column(db.Date, nullable=True) 
+    selected_words = db.Column(MutableList.as_mutable(JSON), nullable=True)
+    jumpsA = db.Column(MutableList.as_mutable(JSON), nullable=True)
+    total_jumps = db.Column(db.Integer, default=0)
+    results = db.Column(MutableList.as_mutable(JSON), nullable=True)
+    prompt_idx = db.Column(db.Integer, nullable=True) # make migration that removes this
+    current_jumps = db.Column(db.Integer, default=0) # make migration that removes this
+    prompts = db.Column(MutableList.as_mutable(JSON), nullable=True) # make migration that removes this
+    start_target_idxs = db.Column(MutableList.as_mutable(JSON), nullable=True, default=[[0,0], [0,5]])
+
+    __table_args__ = (
+        db.UniqueConstraint("user_id", "current_date", name="user_date_uc"),
+    )
+
 class PasswordResetPin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(36), db.ForeignKey("user.id"), nullable=False)
@@ -42,18 +59,18 @@ class PasswordResetPin(db.Model):
     expires_at = db.Column(db.DateTime, nullable=False)
 
 # stores the generated gamestate for the user 
-# class UserConstructedGamestate(db.Model): 
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_creator_id = db.Column(db.String(36), db.ForeignKey("user.id"), nullable=False)
-#     url = db.Column(db.String(36), nullable=True) # this is guaranteed to be unique (should be unique at least)
-#     game_id = db.Column(db.String(36), nullable=False)
-#     user_player_id = db.Column(db.String(36), nullable=False)
-#     current_date = db.Column(db.Date, nullable=True) # this should be date that is generated when another user plays your game 
-#     selected_words = db.Column(MutableList.as_mutable(JSON), nullable=True)
-#     jumpsA = db.Column(MutableList.as_mutable(JSON), nullable=True)
-#     total_jumps = db.Column(db.Integer, default=0)
-#     results = db.Column(MutableList.as_mutable(JSON), nullable=True)
-#     prompt_idx = db.Column(db.Integer, nullable=True) # make migration that removes this
-#     current_jumps = db.Column(db.Integer, default=0) # make migration that removes this
-#     prompts = db.Column(MutableList.as_mutable(JSON), nullable=True) # make migration that removes this
-#     start_target_idxs = db.Column(MutableList.as_mutable(JSON), nullable=True, default=[[0,0], [0,5]])
+class UserConstructedGamestate(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    user_creator_id = db.Column(db.String(36), db.ForeignKey("user.id"), nullable=False)
+    url = db.Column(db.String(36), nullable=True) # this is guaranteed to be unique (should be unique at least)
+    game_id = db.Column(db.String(36), nullable=False)
+    user_player_id = db.Column(db.String(36), nullable=False)
+    current_date = db.Column(db.Date, nullable=True) # this should be date that is generated when another user plays your game 
+    selected_words = db.Column(MutableList.as_mutable(JSON), nullable=True)
+    jumpsA = db.Column(MutableList.as_mutable(JSON), nullable=True)
+    total_jumps = db.Column(db.Integer, default=0)
+    results = db.Column(MutableList.as_mutable(JSON), nullable=True)
+    prompt_idx = db.Column(db.Integer, nullable=True) # make migration that removes this
+    current_jumps = db.Column(db.Integer, default=0) # make migration that removes this
+    prompts = db.Column(MutableList.as_mutable(JSON), nullable=True) # make migration that removes this
+    start_target_idxs = db.Column(MutableList.as_mutable(JSON), nullable=True, default=[[0,0], [0,5]])
