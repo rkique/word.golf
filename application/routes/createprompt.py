d520d32227e1fa0b73e6eeb0e2bc13f5ea683ad9
jb2700 <113483299+jb2700@users.noreply.github.com>
2025-08-01 09:01:34 -0400

    [Misc] stashing incomplete changes

--- application/routes/createprompt.py
+++ application/routes/createprompt.py
@@ -1,6 +1,9 @@
 from ..utils import get_curve, get_furthest_away_word, get_similar_word, find_common_neighbor, similarity
 from flask import Blueprint,render_template, request, make_response, jsonify
 from ..internals import globals
+from ..internals.auth import get_user_from_cookie
+from ..models import UserConstructedGamestate
+from .. import db, oauth, cookie_signer
 
 createprompt_bp = Blueprint('createPrompt', __name__)
 
@@ -24,7 +27,6 @@ def next_word():
 @createprompt_bp.route('/create-prompt', methods=['GET'])
 def create_own_prompt():
     return render_template('create-prompt.html')
-
 
 @createprompt_bp.route('/check-new-prompts', methods=['POST'])
 def check_new_prompts():
@@ -58,3 +60,56 @@ def check_new_prompts():
     return_data["neighbor"] = common_neighbor
 
     return make_response(return_data)
+
+
+@createprompt_bp.route('/generate-gamestate-and-url', methods=['POST'])
+def check_new_prompts():
+    data = request.get_json()
+    if not data or not "prompts" in data:
+        return 
+    user = get_user_from_cookie()
+    if not user:
+        return 
+    
+    base_url = request.host_url.rstrip('/')
+    # Get the max game_id from the database and add 1 to it
+    max_game = UserConstructedGamestate.query.order_by(UserConstructedGamestate.game_id.desc()).first()
+    if max_game:
+        new_game_id = str(int(max_game.game_id) + 1)
+    else:
+        new_game_id = "1"
+
+    new_gamestate = UserConstructedGamestate(
+        user_creator_id=user.id,
+        url=base_url + '/custom?game=' + new_game_id,
+        game_id=new_game_id,
+        user_player_id=user.id,
+        prompts=data["prompts"],
+    )
+
+    db.session.add(new_gamestate)
+    db.session.commit()
+
+    # class UserConstructedGamestate(db.Model): 
+    # id = db.Column(db.Integer, primary_key=True)
+    # user_creator_id = db.Column(db.String(36), db.ForeignKey("user.id"), nullable=False)
+    # url = db.Column(db.String(36), nullable=True) # this is guaranteed to be unique (should be unique at least)
+    # game_id = db.Column(db.String(36), nullable=False)
+    # user_player_id = db.Column(db.String(36), nullable=False)
+    # current_date = db.Column(db.Date, nullable=True) # this should be date that is generated when another user plays your game 
+    # selected_words = db.Column(MutableList.as_mutable(JSON), nullable=False, default=[])
+    # jumpsA = db.Column(MutableList.as_mutable(JSON), nullable=True)
+    # total_jumps = db.Column(db.Integer, default=0)
+    # results = db.Column(MutableList.as_mutable(JSON), nullable=True)
+    # prompt_idx = db.Column(db.Integer, nullable=True) # make migration that removes this
+    # current_jumps = db.Column(db.Integer, default=0) # make migration that removes this
+    # prompts = db.Column(MutableList.as_mutable(JSON), nullable=True) # make migration that removes this
+    # start_target_idxs = db.Column(MutableList.as_mutable(JSON), nullable=True, default=[[0,0], [0,5]])
+    new_url_data = {}
+    new_url_data["url"] = base_url + '/custom?game=' + new_game_id
+    return make_response(new_url_data)
+
+@createprompt_bp.route('/custom', methods=['GET'])
+def check_new_prompts():
+    # use search params to see/determine which 
+    return make_response(render_template('index.html'))
