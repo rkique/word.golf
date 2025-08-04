4db2771fc9bcc1701dfc0ba37fef56d4dca2a6ff
Eric Xia <ericpingxia@gmail.com>
2025-08-04 12:31:40 -0400

    [server] update logging, install eventlet

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from application import create_app, socketio
 from waitress import serve
 import os
@@ -10,8 +12,8 @@ app = create_app()
 if __name__ == "__main__":
     if os.getenv("DEV", "false").lower() == "true":
         port = int(os.getenv("PORT", 8080))
-        socketio.run(app, host="0.0.0.0", port=port, debug=True, use_reloader=False)
+        socketio.run(app, host="0.0.0.0", port=port, debug=True, \
+                     use_reloader=False, log_output=False)
     else:
         port = int(os.getenv("PORT", 8000))
         socketio.run(app, host="0.0.0.0", port=port)
-    print('SocketIO server is running...')
