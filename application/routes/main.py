456e1f43a549bcea6c4823b218a4a669f9e0800e
Eric Xia <ericpingxia@gmail.com>
2025-10-13 10:48:06 -0400

    hotfix main

--- application/routes/main.py
+++ application/routes/main.py
@@ -590,8 +590,7 @@ def get_prompts_for_date(date : datetime.datetime) -> list:
     '''
     Returns a list of ([start,target],neighbor) for the given date.
     '''
-    date = datetime.date(2025, 10, 1)
-    elapsed = elapsed_days(date)
+    elapsed = elapsed_days(date) - 90
     prompt_range = range(elapsed * PROMPT_COUNT, (elapsed + 1) * PROMPT_COUNT)
     return elapsed, [PROMPTS[i] for i in prompt_range], [NEIGHBORS[i] for i in prompt_range]
     
